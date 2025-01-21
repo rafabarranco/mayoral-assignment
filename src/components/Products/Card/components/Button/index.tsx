515e9774b5342fc38778a697d3fed3f8f1610693
Rafa Barranco Vela <rafabarrancovela@gmail.com>
2025-01-21 12:35:23 +0100

    Fix button styles on mobile

--- src/components/Products/Card/components/Button/index.tsx
+++ src/components/Products/Card/components/Button/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 const CardButton: FC = () => (
-  <button className="w-fit bg-blue-600 text-white py-1 px-3 rounded-lg hover:bg-blue-700">
+  <button className="w-fit bg-blue-600 text-white py-1 px-3 rounded-lg hover:bg-blue-700 text-sm">
     AÑADIR
   </button>
 );
